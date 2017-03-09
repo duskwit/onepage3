cb41b710512bf724dc70ddc3a7670296c7de1cd4
Klaudia <duskwit@gmail.com>
2017-03-09 14:25:55 +0100

    finished project

---
+++ js/smoothScroll.js
@@ -0,0 +1,56 @@
+// thumbnail gallery photos
+$(document).ready(function() {
+	$('.thumbnail').click(function(){
+      $('.modal-body').empty();
+    	var title = $(this).parent('a').attr("title");
+    	$('.modal-footer').html(title);
+    	$($(this).parents('div').html()).appendTo('.modal-body');
+    	$('#myModal').modal({show:true});
+	});
+    $('.image a').click(function(e){
+        e.preventDefault();
+    });
+});
+
+
+$(function() {
+  $('.sel_scroll li a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  $('#scrollRocket').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  $('.btn-scroll').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+});
